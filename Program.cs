1b8c6f24fa9bf2fa2ea7467b2856122ba5adea8a
Mauro Pico <sebaspico@outlook.com>
2026-03-09 19:31:30 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioDeber_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Dado un rango inicial y un rango final, calcule la sumatoria de todos los números primos
+            //que terminen en 7.De ser el caso incluye el número 7.
+
+             int rangoInicial = Console.ReadLine().;
+        }
+    }
+}

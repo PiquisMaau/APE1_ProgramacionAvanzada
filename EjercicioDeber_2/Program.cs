b04ce49e9d7cd2f1ec76488a16d7c6b9a09da8be
Mauro Pico <sebaspico@outlook.com>
2026-03-13 09:26:27 -0500

    Restructuración: mover git y solucion a las carpetas correctas

---
+++ EjercicioDeber_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioDeber_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+//Desarrolla un programa en C# que permita almacenar en un array unidimensional las 
+//calificaciones de 10 estudiantes en una evaluación. 
+//  El sistema debe:
+//   • Ingresar las notas validando que estén entre 0 y 10.
+//   • Mostrar todas las calificaciones registradas.
+//   • Calcular el promedio general, la nota más alta y la nota más baja. 
+//   • Indicar cuántos estudiantes aprobaron y cuántos reprobaron.
+//   • Crear una clase llamada Curso que contenga métodos para realizar los cálculos.
+        }
+    }
+}

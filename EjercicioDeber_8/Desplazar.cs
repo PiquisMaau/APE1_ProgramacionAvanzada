4ccfb04dc5cc4b13b14a5e71995729f2ebeb2ecc
Mauro Pico <sebaspico@outlook.com>
2026-03-17 01:55:42 -0500

    Resolucion ejercicio 8

---
+++ EjercicioDeber_8/Desplazar.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioDeber_8
+{
+   
+        public class Desplazador
+        {
+            public void RotarDerecha(int[] arr, int k)
+            {
+                if (arr == null || arr.Length == 0) return;
+
+                k = k % arr.Length;
+                if (k == 0) return;
+
+                Invertir(arr, 0, arr.Length - 1);
+                Invertir(arr, 0, k - 1);
+                Invertir(arr, k, arr.Length - 1);
+            }
+
+            private void Invertir(int[] arr, int inicio, int fin)
+            {
+                while (inicio < fin)
+                {
+                    int temp = arr[inicio];
+                    arr[inicio] = arr[fin];
+                    arr[fin] = temp;
+                    inicio++;
+                    fin--;
+                }
+            }
+        }
+}
---
+++ EjercicioDeber_8/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioDeber_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Implementa una clase Desplazador con un método que reciba un array de enteros y un
+            //número k.El método debe rotar el array k posiciones a la derecha. Por ejemplo[1, 2, 3, 4, 5]
+            //con k = 2 resulta en[4, 5, 1, 2, 3]. Realiza la operación "in-place"(sin crear un array auxiliar de
+            //gran tamaño), optimizando el uso de memoria.
+           
+                Console.WriteLine("----- Desplazador de valores en ARRAYS -----");
+
+                int[] miArray = { 1, 2, 3, 4, 5 };
+
+                int k = 2;
+
+                Console.WriteLine("Array Original:");
+                ImprimirArray(miArray);
+
+                Desplazador desplazador = new Desplazador();
+
+                Console.WriteLine($"Rotando el array {k} posiciones a la derecha de forma inplace...");
+
+                desplazador.RotarDerecha(miArray, k);
+
+                Console.WriteLine("Array Resultante:");
+                ImprimirArray(miArray);
+
+                
+            }
+
+            static void ImprimirArray(int[] arr)
+            {
+                Console.Write("[ ");
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.Write(arr[i]);
+                    if (i < arr.Length - 1) Console.Write("| ");
+                }
+                Console.WriteLine(" ]");
+            }
+        }
+    }
+

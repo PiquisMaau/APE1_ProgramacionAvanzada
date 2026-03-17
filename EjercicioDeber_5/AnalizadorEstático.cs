3ed485540a25d0ab8faedbc7420385764055fab7
Mauro Pico <sebaspico@outlook.com>
2026-03-17 01:05:41 -0500

    Resolucion ejercicio 5.

---
+++ EjercicioDeber_5/AnalizadorEstático.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioDeber_5
+{
+    internal class AnalizadorEstadistico
+    {
+        private double[] datos;
+
+        public AnalizadorEstadistico(double[] datos)
+        {
+            this.datos = datos; 
+        }
+
+        public double CalcularMedia()
+        {
+            double sumatoria = 0;
+            foreach (var dato in datos) sumatoria += dato;
+            return sumatoria / datos.Length;
+        }
+
+        public double CalcularMediana()
+        {
+            double[] copia = new double[datos.Length];
+            Array.Copy(datos, copia, datos.Length);
+            Array.Sort(copia);
+
+            int mitad = copia.Length / 2;
+            if (copia.Length % 2 == 0)
+                return (copia[mitad - 1] + copia[mitad]) / 2.0;
+            else
+                return copia[mitad];
+        }
+
+        public double CalcularModa()
+        {
+            var grupos = datos.GroupBy(x => x).OrderByDescending(g => g.Count());
+            return grupos.First().Key;
+        }
+
+        public double CalcularDesviacionEstandar()
+        {
+            double media = CalcularMedia();
+            double sumaCuadrados = 0;
+            foreach (var d in datos)
+            {
+                sumaCuadrados += Math.Pow(d - media, 2);
+            }
+            return Math.Sqrt(sumaCuadrados / datos.Length);
+        }
+    }
+}
---
+++ EjercicioDeber_5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioDeber_5
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            //Crea una clase AnalizadorEstadistico que reciba un array de números reales(double[]) en
+            //su constructor.Implementa métodos para calcular la media, la mediana, la moda y la
+            //desviación estándar.El método de la mediana debe ordenar el array internamente sin
+            ////modificar el array original(usar Array.Copy).
+            Console.Write("Ingresa el tamaño de tu array: ");
+            int tamanio = int.Parse(Console.ReadLine());
+            double[] datosAnalizador = new double[tamanio];
+            Random random = new Random();
+            for (int i = 0; i < datosAnalizador.Length; i++)
+            {
+                datosAnalizador[i] = Math.Round(random.NextDouble()*(1000-(-1000))+(-1000), 2);
+            }
+            Console.WriteLine("----- Tu array es: ");
+            foreach (var item in datosAnalizador)
+            {
+                Console.Write(item + " | ");
+            }
+            Console.WriteLine();
+            AnalizadorEstadistico an1 = new AnalizadorEstadistico(datosAnalizador);
+            Console.WriteLine($"La mediana de tu array es: {an1.CalcularMediana()}");
+            Console.WriteLine($"La Desviación Estándar de tu array es: {an1.CalcularDesviacionEstandar()}");
+            Console.WriteLine($"La Media de tu array es: {an1.CalcularMedia()}");
+            Console.WriteLine($"La Moda de tu array es: {an1.CalcularModa()}");
+
+        }
+    }
+}

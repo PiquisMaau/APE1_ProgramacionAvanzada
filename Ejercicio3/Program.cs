962c62b6d8b5e0a9bc0880d9054d18d2afca7c28
Mauro Pico <sebaspico@outlook.com>
2026-03-16 19:13:35 -0500

    Ejercicio terminado y con todos los metodos y analisis modulados en clases.

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    internal class Program : TemperaturasMes
+    {
+        static void Main(string[] args)
+        {
+            //            Diseña una aplicación que registre en un array unidimensional las temperaturas diarias de
+            //un mes de 31 días.
+            //El programa debe:
+            //• Permitir el ingreso de temperaturas(utilice una carga de datos randómica).
+            //• Mostrar cuántos días tuvieron temperatura superior al promedio. 
+            //• Buscar si una temperatura específica ingresada por el usuario existe en el array.
+            //• Mostrar en qué posiciones se encuentra. 
+            //• Organizar la lógica en una clase TemperaturaMes con métodos de búsqueda y
+            //análisis.
+
+            Console.WriteLine("------- Temperaturas diarias ---------");
+            double[] arrayTemperaturas = new double[31];
+            double temGenerada = 0;
+            Random temperaturasRandom = new Random();
+            for (int i = 0; i < arrayTemperaturas.Length; i++)
+            {
+                temGenerada = (temperaturasRandom.NextDouble() * (40.0 - 10.0) + 10.0);
+                arrayTemperaturas[i] = Math.Round(temGenerada, 2);
+            }
+
+            ImprimirTemperaturas(arrayTemperaturas);
+            ObtenerPromedioyComparar(arrayTemperaturas);
+            ObtenerTemperaturasSolicitadas(arrayTemperaturas);
+
+        }
+
+       
+    }
+}
---
+++ Ejercicio3/TemperaturasMes.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    internal class TemperaturasMes
+    {
+        public static void ObtenerTemperaturasSolicitadas(double[] arrayTemperaturas)
+        {
+            //Método para buscar una temperatura pedida por el usuario 
+            Console.WriteLine("---------------------------------------------");
+            Console.Write("Ingresa una temperatura para buscar: ");
+            double temperaturaIngresada = double.Parse(Console.ReadLine());
+            bool encontrado = false;
+            for (int i = 0; i < arrayTemperaturas.Length; i++)
+            {
+                if (arrayTemperaturas[i] == temperaturaIngresada)
+                {
+                    Console.WriteLine($"La temperatura ingresada sí existe en la posición: {(i + 1)}. {arrayTemperaturas[i]}");
+                    encontrado = true;
+                }
+            }
+            if (!encontrado) { Console.WriteLine("La temperatura ingresada no existe en el array."); }
+        }
+
+        public static void ObtenerPromedioyComparar(double[] arrayTemperaturas)
+        {
+            //Método para calcular el mayor al promedio y mostrar la cantidad que supera esta cifra
+            double sumatoria = 0, promedio;
+            for (int i = 0; i < arrayTemperaturas.Length; i++)
+            {
+                sumatoria += arrayTemperaturas[i];
+            }
+            promedio = sumatoria / arrayTemperaturas.Length;
+            Console.WriteLine($"Las temperaturas mayores al promedio({Math.Round(promedio, 2)}) son: ");
+            int contador = 0;
+            for (int i = 0; i < arrayTemperaturas.Length; i++)
+            {
+                if (promedio < arrayTemperaturas[i])
+                {
+                    Console.Write(Math.Round(arrayTemperaturas[i], 2) + " - ");
+                    contador++;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Hay {contador} temperaturas superiores al promedio.");
+        }
+
+        public static void ImprimirTemperaturas(double[] arrayTemperaturas)
+        {
+            //Método para imprimir
+            for (int i = 0; i < arrayTemperaturas.Length; i++)
+            {
+                Console.WriteLine(Math.Round(arrayTemperaturas[i], 2) + " ");
+            }
+        }
+    }
+}
